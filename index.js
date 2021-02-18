f5c6c1b30b355b9ccc619b1f7936166ff7646e09
DGorgula <gil@dankner.org>
2021-02-18 08:39:00 +0200

    id check created
    id check created for relevant communication functions.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/allData', (request, response) => {
     const DataFilesContent = {'my-todo': []};
     const dataFolderFiles = fs.readdirSync(path);
     const filteredFiles = dataFolderFiles.filter(file => {
-        return (/([a-z0-9]{8})-([a-z0-9]{4})-([a-z0-9]{4})-([a-z0-9]{4})-([a-z0-9]{12}).json/).test(file);
+        return isValidId(file);
     })
     filteredFiles.forEach( file=>{
         const content = fs.readFileSync(path+file);
@@ -24,17 +24,22 @@ app.get('/allData', (request, response) => {
 
 app.get('/:id', (request, response) => {
     const {id} = request.params;
+    if (!isValidId(id)) {
+        updateStatus = false;
+        response.send(updateStatus);
+        return;
+    }
     const reponseContent = fs.readFileSync(`./${id}.json`);
     response.send(reponseContent);
 })
 
 app.post('/add', (request, response) => {
     const idValue = uuid();
-    let addingStatus = true
+    let addStatus = true
     const content = request.body;
     content.id = idValue;
     const jsonedContent = JSON.stringify(content);
-
+    
     fs.writeFile(`${path+idValue}.json` , jsonedContent, () => {});
     response.send(addingStatus);
     console.log(request.body);
@@ -43,16 +48,38 @@ app.post('/add', (request, response) => {
 
 app.put('/edit/:id', (request, response) => {
     const {id} = request.params;
+    let updateStatus = true
+    if (!isValidId(id)) {
+        updateStatus = false;
+        response.send(updateStatus);
+        return;
+    }
     const rawOldContent = fs.readFileSync(`${path+id}.json`);
     const oldContent = JSON.parse(rawOldContent);
     const content = request.body;
-for (let property in content) {
-    oldContent[property] = content[property];
-   }
+    for (let property in content) {
+        oldContent[property] = content[property];
+    }
     const jsonedContent = JSON.stringify(oldContent);
     fs.writeFileSync(`${path+id}.json`, jsonedContent);
-    response.send(`Thank you for the contribution. ${request.body}`);
+    response.send(updateStatus);
+});
+
+app.delete('/delete/:id', (request, response) => {
+    const {id} = request.params;
+    let deleteStatus = true
+    if (!isValidId(id)) {
+        deleteStatus = false;
+        response.send(deleteStatus);
+        return;
+    }
+    fs.unlinkSync(`./${id}.json`);
+    response.send(deleteStatus);
 });
 
 
 app.listen(3000, ()=> {console.log("connected to port 3000");});
+
+function isValidId(fileName) {
+   return (/([a-z0-9]{8})-([a-z0-9]{4})-([a-z0-9]{4})-([a-z0-9]{4})-([a-z0-9]{12})/).test(fileName);
+}
